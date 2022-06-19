229bd94234b86999f7cd374c2f244ac047306647
DharshanRaghuraman <dharshanraghuraman2005@gmail.com>
2022-06-20 01:25:02 +0400

    updated

--- matrix.py
+++ matrix.py
@@ -1,15 +1,19 @@
-def matrix(m, n):
+def matrix(m=int(input("Number of Columns")), n=(int(input("Number of Rows")))):
     L1 = []
-    for i in range(m):
+    for i in range(n):
         L2 = []
-        for j in range(n):
-            ele = int(input(f"Enter Element of (Colomn,row) -> ({i+1},{j+1}): "))
+        for j in range(m):
+            ele = int(input(f"Enter Element of (row,column) -> ({i+1},{j+1}): "))
             L2.append(ele)
         L1.append(L2)
+    print("Matrix Created Successfully")
+    display_matrix(L1)
     return L1
+    
 
-
-def matrix_addition(m1, m2):
+def matrix_addition(m1):
+    print("Create Matrix to Add with:")
+    m2=matrix()
     if len(m1) == len(m2) and len(m1[0]) == len(m2[0]):
         l1 = []
         for i in range(len(m1)):
@@ -18,12 +22,16 @@ def matrix_addition(m1, m2):
                 a = m1[i][j]+m2[i][j]
                 l2.append(a)
             l1.append(l2)
+        print("Matrices Added Successfully")
+        display_matrix(l1)
         return l1
     else:
         print("The matrices provided are not of the same order, can't be added ")
 
 
-def matrix_subtraction(m1, m2):
+def matrix_subtraction(m1):
+    print("Create Matrix to Subtract with:")
+    m2=matrix()
     if len(m1) == len(m2) and len(m1[0]) == len(m2[0]):
         l1 = []
         for i in range(len(m1)):
@@ -32,12 +40,16 @@ def matrix_subtraction(m1, m2):
                 a = m1[i][j]-m2[i][j]
                 l2.append(a)
             l1.append(l2)
+        print("Matrices Subtracted Successfully")
+        display_matrix(l1)
         return l1
     else:
-        print("The matrices provided are not of the same order, can't be subtracted ")
+        print("The matrices provided are not of the same order, Hence can't be subtracted ")
 
 
-def matrix_multiplication(m1, m2):
+def matrix_multiplication(m1):
+    print("Create Matrix to Multiply with:")
+    m2=matrix()
     if len(m1[0]) == len(m2):
         l1 = []
         for i in range(len(m1)):
@@ -48,35 +60,61 @@ def matrix_multiplication(m1, m2):
                     a += m1[i][k]*m2[k][j]
                 l2.append(a)
             l1.append(l2)
+        print("Matrices Multiplied Successfully")
+        display_matrix(l1)
+        return l1
+    else:
+        print("Matrix can't be mutiplied, No of row of first matrix should be equal to no of column of second matrix")
+
+
+def list_for_detreminat(m):
+    if len(m)==len(m[0]):
+        l1 = []
+        for i in range(len(m)):
+            for j in range(len(m[0])):
+                l2=[]
+                for k in range(len(m)):
+                    if k != i:
+                        l3 = []
+                        for l in range(len(m[0])):
+                            if l != j:
+                                l3.append(m[k][l])
+                        l2.append(l3)
+                l1.append(l2)
         return l1
 
 
-def determinant(m):
-    if len(m) == 1:
-        d = m[0][0]
-        return d
-    elif len(m) == 2:
-        d = m[0][0] * m[1][1] - m[0][1] * m[1][0]
-        return d
-    elif len(m) == 3:
-        a = m[0][0]
-        b = m[0][1]
-        c = m[0][2]
-
-        m1 = [[m[1][1], m[1][2]],
-              [m[2][1], m[2][2]]]
-        
-        m2 = [[m[1][0], m[1][2]],
-              [m[2][0], m[2][2]]]
-        
-        m3 = [[m[1][0], m[1][1]],
-              [m[2][0], m[2][1]]]
-        
-        d = a*determinant(m1) - b*determinant(m2) + c*determinant(m3)
-        return d
+def minors_of_matrix(m):
+    if len(m)==len(m[0]):
+        l=list_for_detreminat(m)
+        l1=[]
+        for i in range(len(m)):
+            for j in range(len(m[0])):
+                d=determinant_of_matrix(l[i][j])
+                print(l[i][j])
+                l1.append(d)
+        print(l1)
+    else:
+        print("Minor can't be found, no of rows and no of columns must be equal")
 
 
-def transpose(m):
+def cofactor_matrix(m):
+    if len(m)==len(m[0]):
+        l=list_for_detreminat(m)
+        l1=[]
+        for i in range(len(m)):
+            l2=[]
+            for j in range(len(m[0])):
+                d=determinant_of_matrix(l[i][j])
+                if (i+j)%2==0:
+                    l2.append(d)
+                else:
+                    l2.append(-d)
+            l1.append(l2)
+        return l1
+
+
+def transpose_of_matrix(m):
     l1=[]
     for i in range(len(m[0])):
         l2=[]
@@ -86,6 +124,50 @@ def transpose(m):
     return l1
 
 
+def adjoint_of_matrix(m):
+    if len(m)==len(m[0]):
+        a=cofactor_matrix(m)
+        adj=transpose_of_matrix(a)
+        return adj
+    else:
+        print("Adjoint of the matrix can't be found, no of rows and no of columns must be equal")
+
+def determinant_of_matrix(m):
+    if len(m)==len(m[0]):
+        if len(m) == 1:
+            d = m[0][0]
+
+        elif len(m) == 2:
+            d = m[0][0] * m[1][1] - m[0][1] * m[1][0]
+            
+        else:
+            a=minors_of_matrix(m)
+            d=0
+            for i in range(len(m)):
+                if i%2==0:
+                    d+=m[0][i]*determinant_of_matrix(a[i])
+                else:
+                    d-=m[0][i]*determinant_of_matrix(a[i])
+            print(f"Determinat of the Given Matrix is: {d}")
+        return d
+    else:
+        print("Determinat can't be found, no of rows and no of columns must be equal")
+
+
+def inverse_of_matrix(m):
+    if len(m)==len(m[0]):
+        a=cofactor_matrix(m)
+        b=determinant_of_matrix(m)
+        l1=[]
+        for i in range(len(a[0])):
+            l2=[]
+            for j in range(len(a)):
+                l2.append(a[j][i]/b)
+            l1.append(l2)
+    else:
+        print("Inverse can't be found, no of rows and no of columns must be equal")
+
+
 def display_matrix(m):
     for i in range(len(m)):
         print("[", end=" ")
@@ -93,3 +175,4 @@ def display_matrix(m):
             print(m[i][j], end=" ")
         print("]")
 
+matrix()
--- menu_driven.py
+++ menu_driven.py
@@ -1,49 +1,81 @@
+from types import NoneType
 from matrix import *
 
-print("Matrix 1: ")
-m1 = matrix(int(input("Enter Number of Colomn: ")),int(input("Enter Number of Row: ")))
-print("\nMatrix 2: ")
-m2 = matrix(int(input("Enter Number of Colomn: ")),int(input("Enter Number of Row: ")))
-print('\nMatrix 1 is :')
-display_matrix(m1)
-print('\nMatrix 2 is :')
-display_matrix(m2)
-
+print("\nWelcome to Matrix Calaculator\n\nCreate Matrix:")
+m1 = matrix(int(input("\nEnter Number of Colomn: ")),int(input("Enter Number of Row: ")))
 while True:
-    choice = int(input("\nEnter choice: "))
-    if choice == 1:
-        m3 = matrix_addition(m1, m2)
-        display_matrix(m3)
+    print('''\n
+    Choose one of the following:
 
+    1. Matrix Addition
+    2. Matrix Subtraction
+    3. Matrix Multiplication
+    4. Minor/Minors of a Matrix 
+    5. Cofactor Matrix
+    6. Transpose of a Matrix
+    7. Adjoint of a Matrix
+    8. Determinant of a Matrix
+    9. Inverse of a Matrix
+    0. Quit        
+    ''')
+    choice = int(input("\nEnter choice: "))
+
+    if choice == 1:
+        matrix_addition(m1)
+    
     elif choice == 2:
-        m3 = matrix_subtraction(m1, m2)
-        display_matrix(m3)
+        matrix_subtraction(m1)
 
     elif choice == 3:
-        m3 = matrix_multiplication(m1, m2)
-        display_matrix(m3)
-
+        matrix_multiplication(m1)
+    
     elif choice == 4:
-        print('\nDeterminant of')
-        display_matrix(m1)
-        print(f'is: {determinant(m1)}')
-       
-
-        print('\nDeterminant of')
-        display_matrix(m2)
-        print(f'is: {determinant(m2)}')
-        
-
+        minors_of_matrix(m1)
+    
     elif choice == 5:
+        cofactor=cofactor_matrix(m1)
+        if cofactor is not NoneType:
+            print('\nCofactor Matrix of')
+            display_matrix(m1)
+            print('is: ')
+            display_matrix(cofactor)
+        else:
+            print("\nCofactor Matrix can't be found, no of rows and no of columns must be equal")
+    
+    elif choice == 6:
         print('\nTranspose of')
         display_matrix(m1)
         print('is: ')
-        display_matrix(transpose(m1))
+        display_matrix(transpose_of_matrix(m1))
 
-        print('Transpose of')
-        display_matrix(m2)
-        print('is: ')
-        display_matrix(transpose(m2))
+    elif choice == 7:
+        adjoint=adjoint_of_matrix(m1)
+        if adjoint is not NoneType:
+            print('\nAdjoint Matrix of the matrix')
+            display_matrix(m1)
+            print('is: ')
+            display_matrix(adjoint)
+        else:
+            print("\nAdjoint Matrix can't be found, no of rows and no of columns must be equal")
 
-    elif choice == 0:
+    elif choice == 8:
+        determinant=determinant_of_matrix(m1)
+        if determinant is not NoneType:
+            print('\nDeterminat of the Matrix')
+            display_matrix(m1)
+            print(f'is: {determinant}')
+        else:
+            print("\nDeterminant of the Matrix can't be found, no of rows and no of columns must be equal")
+    
+    elif choice == 9:
+        inverse=inverse_of_matrix(m1)
+        if inverse is not NoneType:
+            print('\nInverse of the Matrix')
+            display_matrix(m1)
+            print('is: ')
+            display_matrix(inverse)
+        else:
+            print("\nDeterminant of the Matrix can't be found, no of rows and no of columns must be equal")
+
+    else:
         break
